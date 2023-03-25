81db3656ca3b793216c4ed041b624d9f2859c860
Ali Ahasan <gamer.aliahasan@gmail.com>
2023-03-25 18:43:20 +0600

    Settings changed and first test was deployed

--- base/settings.py
+++ base/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'todo',
+    'home',
+    'access',
+    'about',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
