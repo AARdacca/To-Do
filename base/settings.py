dfc167d3e05737928dbc2e28ef67534a22b5ca79
Ali Ahasan <gamer.aliahasan@gmail.com>
2023-03-27 01:06:22 +0600

    Added CSS to Base but If Else needed for NavBar for CurrentPage Text Highlight additionally Merged About app and Home app into Navigation app for more Lighter experience

--- base/settings.py
+++ base/settings.py
@@ -32,9 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'todo',
-    'home',
     'access',
-    'about',
+    'navigation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Manual STATIC add
+STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+    BASE_DIR / "base/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- navigation/urls.py
+++ navigation/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from navigation import views
+from navigation.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('navigation.urls')),
-    path('', include('access.urls')),
-    path('', include('todo.urls')),
+    # path('admin/', admin.site.urls),
+    path('home/', views.home, name = 'home'),
+    path('about/', views.about, name = 'about'),
 ]
