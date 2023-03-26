8d667a8eb5bac7e1346c85e3ba4a93064fd3c339
Ali Ahasan <gamer.aliahasan@gmail.com>
2023-03-27 01:14:05 +0600

    Deleted Home app and About app Additionally deleted search button which will be added later after it is functional

--- about/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.forms import inlineformset_factory
-from django.contrib.auth.forms import UserCreationForm
-
-from.models import*
-
-def about(request):
-    return render(request,'about/about.html') 
--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.forms import inlineformset_factory
-from django.contrib.auth.forms import UserCreationForm
-
-from.models import*
-
-def home(request):
-    return render(request,'home/home.html') 
